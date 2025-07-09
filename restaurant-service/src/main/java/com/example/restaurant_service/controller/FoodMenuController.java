faef45a322076c475b2d8296e252412c49285e9c
Huvison <huvisoncollins@gmail.com>
2025-07-09 16:20:14 +0000

    chore(feat/enhancement): enhance restaurant-service with advanced logging using sl4j

--- restaurant-service/src/main/java/com/example/restaurant_service/controller/FoodMenuController.java
+++ restaurant-service/src/main/java/com/example/restaurant_service/controller/FoodMenuController.java
@@ -1,15 +1,21 @@
 package com.example.restaurant_service.controller;
 
 import com.example.restaurant_service.dto.ApiResponseDto;
+import com.example.restaurant_service.dto.foodMenuDto.requestDto.FoodMenuRequest;
 import com.example.restaurant_service.dto.foodMenuDto.requestDto.FoodMenuUpdateRequest;
 import com.example.restaurant_service.dto.foodMenuDto.responseDto.FoodMenuResponse;
 import com.example.restaurant_service.service.serviceInterfaces.FoodMenuService;
 import com.example.restaurant_service.utils.FoodMenuSort;
+import jakarta.validation.Valid;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.*;
 
+@Slf4j
 @RestController
 @RequestMapping("/api/v1/restaurant")
 public class FoodMenuController {
@@ -21,36 +27,45 @@ public class FoodMenuController {
 
     @GetMapping("/{restaurantId}/menu")
     public ResponseEntity<ApiResponseDto<Page<FoodMenuResponse>>> getRestaurantMenu(@PathVariable long restaurantId,
-                                                                                    @RequestParam(required = false, defaultValue = "0") int pageNumber,
-                                                                                    @RequestParam(required = false, defaultValue = "SORT_BY_NAME") FoodMenuSort sortField){
+                                                                                    @Valid @RequestParam(required = false, defaultValue = "0") int pageNumber,
+                                                                                    @Valid @RequestParam(required = false, defaultValue = "SORT_BY_NAME") FoodMenuSort sortField,
+                                                                                    Authentication authenticatedUser){
+        log.info("Fetching all food menus for restaurant with id: {} and sortField: {} for user with id: {}",restaurantId,sortField.getField(), authenticatedUser.getPrincipal());
         return foodMenuService.getAllFoodMenus(restaurantId,pageNumber,sortField.getField());
     }
 
     @GetMapping("/{restaurantId}/menu/{menuId}")
     public ResponseEntity<ApiResponseDto<FoodMenuResponse>> getFoodMenuById(@PathVariable long restaurantId,
-                                                            @PathVariable long menuId){
+                                                                            @PathVariable long menuId,
+                                                                            Authentication authenticatedUser){
+        log.info("Fetching food menu with id: {} for user with id: {}",menuId,authenticatedUser.getPrincipal());
         return foodMenuService.getFoodMenuById(menuId,restaurantId);
     }
 
     @PreAuthorize("hasRole('RESTAURANT_OWNER')")
     @PostMapping("/{restaurantId}/menu")
     public ResponseEntity<ApiResponseDto<FoodMenuResponse>> addFoodMenu(@PathVariable long restaurantId,
-                                                        @RequestBody com.example.restaurant_service.dto.foodMenuDto.requestDto.FoodMenuRequest foodMenuRequest){
+                                                                        @Valid @RequestBody FoodMenuRequest foodMenuRequest,
+                                                                        Authentication authenticatedUser){
+        log.info("Adding food menu with name: {} for restaurant with id: {} for user with id: {}",foodMenuRequest.name(),restaurantId, authenticatedUser.getPrincipal());
         return foodMenuService.addFoodMenu(restaurantId,foodMenuRequest);
     }
 
     @PreAuthorize("hasRole('RESTAURANT_OWNER') and @resourceOwner.isFoodMenuOwner(#menuId,#authentication)")
     @PatchMapping("/{restaurantId}/menu/{menuId}")
-    public ResponseEntity<ApiResponseDto<FoodMenuResponse>> updateFoodMenu(@RequestBody FoodMenuUpdateRequest foodMenuUpdateRequest,
-                                                            @PathVariable long restaurantId,
-                                                            @PathVariable long menuId){
+    public ResponseEntity<ApiResponseDto<FoodMenuResponse>> updateFoodMenu(@Valid @RequestBody FoodMenuUpdateRequest foodMenuUpdateRequest,
+                                                                           @PathVariable long restaurantId,
+                                                                           @PathVariable long menuId,
+                                                                           Authentication authenticatedUser){
+        log.info("Updating food menu item with if: {} for restaurant with id: {}, and user with id: {}",menuId,restaurantId,authenticatedUser.getPrincipal());
         return foodMenuService.updateFoodMenu(foodMenuUpdateRequest,restaurantId,menuId);
     }
 
     @PreAuthorize("hasRole('RESTAURANT_OWNER') and @resourceOwner.isFoodMenuOwner(#menuId,#authentication)")
     @DeleteMapping("/{restaurantId}/menu/{menuId}")
     public ResponseEntity<ApiResponseDto<String>> deleteFoodMenu(@PathVariable long restaurantId,
-                                                  @PathVariable long menuId){
+                                                                 @PathVariable long menuId, Authentication authenticatedUser){
+        log.info("Deleting food menu item with id: {} for restaurant with id: {}, and user with id: {}",menuId,restaurantId,authenticatedUser.getPrincipal());
         return foodMenuService.deleteFoodMenu(restaurantId,menuId);
     }
 }
--- restaurant-service/src/main/java/com/example/restaurant_service/service/serviceImpl/FoodMenuServiceImpl.java
+++ restaurant-service/src/main/java/com/example/restaurant_service/service/serviceImpl/FoodMenuServiceImpl.java
@@ -11,6 +11,7 @@ import com.example.restaurant_service.model.Restaurant;
 import com.example.restaurant_service.repository.FoodMenuRepository;
 import com.example.restaurant_service.repository.RestaurantRepository;
 import com.example.restaurant_service.service.serviceInterfaces.FoodMenuService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
@@ -21,6 +22,7 @@ import org.springframework.stereotype.Service;
 
 import java.util.Objects;
 
+@Slf4j
 @Service
 public class FoodMenuServiceImpl implements FoodMenuService {
     private final FoodMenuRepository foodMenuRepository;
@@ -38,8 +40,10 @@ public class FoodMenuServiceImpl implements FoodMenuService {
             Restaurant restaurant = restaurantRepository.findById(restaurantId).orElseThrow(() -> new RestaurantException("Restaurant Not Found with id: " + restaurantId));
             FoodMenu menu = FoodMenuMapper.toFoodMenu(foodMenuRequest);
             menu.setRestaurant(restaurant);
+            log.info("Food Menu Saved Successfully with id: {} for restaurant with id: {}",menu.getId(),restaurant.getId());
             return ResponseEntity.ok(ApiResponseDto.success(FoodMenuMapper.toFoodMenuResponse(foodMenuRepository.save(menu)), HttpStatus.CREATED.value()));
         }else{
+            log.info("Unable to save food menu with null request.");
             throw new RestaurantException("Food Menu Request is Null");
         }
     }
@@ -48,6 +52,7 @@ public class FoodMenuServiceImpl implements FoodMenuService {
     public ResponseEntity<ApiResponseDto<FoodMenuResponse>> getFoodMenuById(long id,long restaurantId) {
         Restaurant restaurant = restaurantRepository.findById(restaurantId).orElseThrow(() -> new RestaurantException("Restaurant Not Found with id: " + restaurantId));
         FoodMenu menu = foodMenuRepository.findByIdAndRestaurant(id,restaurant).orElseThrow(() -> new RestaurantException("Menu item Not Found with id: " + id));
+        log.info("Food Menu Found with id: {} for restaurant with id: {}",menu.getId(),restaurant.getId());
         return ResponseEntity.ok(ApiResponseDto.success(FoodMenuMapper.toFoodMenuResponse(menu), HttpStatus.OK.value()));
     }
 
@@ -58,7 +63,7 @@ public class FoodMenuServiceImpl implements FoodMenuService {
         Pageable pageable = PageRequest.of(pageNumber, paginateBy, sort);
 
         Restaurant restaurant = restaurantRepository.findById(restaurantId).orElseThrow(() -> new RestaurantException("Restaurant Not Found with id: " + restaurantId));
-
+        log.info("Fetching all food menus for restaurant with id: {} and sortField: {} ",restaurantId, sortField);
         return ResponseEntity.ok(ApiResponseDto.success(foodMenuRepository.findAllByRestaurant(restaurant,pageable).map(FoodMenuMapper::toFoodMenuResponse), HttpStatus.OK.value())) ;
     }
 
@@ -71,7 +76,7 @@ public class FoodMenuServiceImpl implements FoodMenuService {
         if(foodMenuUpdateRequest.name() != null) menu.setName(foodMenuUpdateRequest.name());
         if(foodMenuUpdateRequest.description() != null) menu.setDescription(foodMenuUpdateRequest.description());
         if (foodMenuUpdateRequest.price() != null) menu.setPrice(foodMenuUpdateRequest.price());
-
+        log.info("Food Menu Updated Successfully with id: {} for restaurant with id: {}",menu.getId(),restaurant.getId());
         return ResponseEntity.ok(ApiResponseDto.success(FoodMenuMapper.toFoodMenuResponse(foodMenuRepository.save(menu)),HttpStatus.OK.value()));
     }
 
@@ -81,6 +86,7 @@ public class FoodMenuServiceImpl implements FoodMenuService {
         FoodMenu menu = foodMenuRepository.findByIdAndRestaurant(id,restaurant).orElseThrow(() -> new RuntimeException("Menu item Not Found with id: " + id));
 
         foodMenuRepository.delete(menu);
+        log.info("Food Menu Deleted Successfully with id: {} for restaurant with id: {}",menu.getId(),restaurant.getId());
         return ResponseEntity.ok(ApiResponseDto.success("Menu Deleted Successfully", HttpStatus.OK.value()));
     }
 }
